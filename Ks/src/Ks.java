192f83c328413f90f8f5df3e5c989fc8cdfbbfd4
ivadimn <ivadimn@mail.ru>
2016-07-13 17:55:34 +0300

    новый проект

---
+++ Ks/src/Ks.java
@@ -0,0 +1,17 @@
+/**
+ * Created by vadim on 13.07.16.
+ */
+public class Ks {
+
+    private static int[][] obhod = {
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8},
+            {0, 3, 6},
+            {1, 4, 7},
+            {2, 5, 8},
+            {0, 4 ,8},
+            {2, 4, 6}
+    };
+    private static int[] field = new int[8];
+}

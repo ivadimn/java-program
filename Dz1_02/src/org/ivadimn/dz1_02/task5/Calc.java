64f6ea9d1c8632742029db37a343c165395eb9e2
ivadimn <ivadimn@mail.ru>
2016-07-13 13:42:23 +0300

    выполнение задание 5

--- Dz1_02/src/org/ivadimn/dz1_02/task5/Calc.java
+++ Dz1_02/src/org/ivadimn/dz1_02/task5/Calc.java
@@ -18,16 +18,40 @@ public class Calc {
             operand1 = console.nextDouble();
             oper = console.next();
             operand2 = console.nextDouble();
-            System.out.println("Вы ввели: " + operand1 + " " + oper + " " + operand2);
+            System.out.println("Значение выражения: " + operand1 + " " + oper + " " + operand2 + " = "
+                    + getResult(operand1, operand2, oper) );
 
             System.out.println("Продолжить? (y/n)");
 
-        } while(!console.next().equals("n"));
+        } while(!console.next().equalsIgnoreCase("n"));
 
     }
 
+    /**
+     * Метод вычисляет значение выражения (операции : " + "; " - "; " * "; " / ")
+     * @param op1
+     * @param op2
+     * @param op
+     * @return double;
+     */
     private static double getResult(double op1, double op2, String op) {
-
-        
+        double result;
+        switch(op) {
+            case "+":
+                result = op1 + op2;
+                break;
+            case "-":
+                result = op1 - op2;
+                break;
+            case "*" :
+                result = op1 * op2;
+                break;
+            case "/":
+                result = op1 / op2;
+                break;
+            default:
+                result = Double.NaN;
+        }
+        return result;
     }
 }

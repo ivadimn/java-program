e9dfb09b31e49255df4cabc3c09faa7accac1f07
ivadimn <ivadimn@mail.ru>
2016-07-20 18:32:09 +0300

    новые редакции

--- Sq/src/org/ivadimn/sq/ui/Main.java
+++ Sq/src/org/ivadimn/sq/ui/Main.java
@@ -2,6 +2,8 @@ package org.ivadimn.sq.ui;
 
 import org.ivadimn.sq.model.Rectangle;
 
+import java.util.ArrayList;
+
 /**
  * Created by vadim on 14.07.16.
  */
@@ -9,47 +11,51 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Rectangle[] rects = new Rectangle[6];
-        int s = 0;
-        // TODO Auto-generated method stub
-            /*if (args.length != 2) {
-                System.out.println("Èñïîëüçîâàíèå:");
-                System.out.println("CalcSquqre input.txt output.txt");
-                System.out.println("input.txt - âõîäíîé ôàéë ñ êîîðäèíàòàìè ïðÿìîóãîëüíèêîâ");
-                System.out.println("output.txt - âûõîäíîé ôàéë, ñîäåðæàùèé ïîëùàäü îáúåäèíåíèÿ ïðÿìîóãîëüíèêîâ");
-                return;
-            }
-            System.out.println(args[0] + " " + args[1]);*/
+        Rectangle[] rects = new Rectangle[3];
+        ArrayList<Rectangle> inters = new ArrayList<>();
 
-        rects[0] = new Rectangle((0, 0, 15, 20);
-        rects[1] = new Rectangle(2,14,5,18);
-        rects[2] = new Rectangle(1, 1, 4, 5);
-        rects[3] = new Rectangle(3, 4,6, 8);
-        rects[4] = new Rectangle(4, 6, 7, 10);
-        rects[5] = new Rectangle(5,7, 8, 11);
-        int s1;
-        int s2;
+        int s = 0;
+
+
+        rects[0] = new Rectangle(0, 0, 5, 7);
+        rects[1] = new Rectangle(1, 1, 6, 8);
+        rects[2] = new Rectangle(2, 2, 7, 9);
+        //rects[3] = new Rectangle(3, 4,6, 8);
+        //rects[4] = new Rectangle(4, 6, 7, 10);
+        //rects[5] = new Rectangle(5,7, 8, 11);
+        int sMinus = 0;
+        int sPlus = 0;
         int ss = 0;
-        for (int k = 0; k < rects.length; k++)
-        s = s + rects[k].getSquare();
-        System.out.println("Ïëîùàäü îáúåäèíåíèÿ - " + s);
+        for (int k = 0; k < rects.length; k++) {
+            s = s + rects[k].getSquare();
+            System.out.println("Площадь  - " + rects[k].getSquare());
+        }
+        System.out.println("Площадь прямоугольников - " + s);
         for (int i = 0; i < rects.length; i++) {
             for(int j = i + 1; j < rects.length; j++) {
 
                 if (j != i) {
-                    Rectangle r = r[i].getIntersection(rects[j]);
-                    ss = ss + s2;
-                }
-                //for (int k = 0; k < i; k++) {
-                //ss = ss - rects[j].getIntersectionSquare(rects[k]);
-                //}
-                //s = s - ss;
-                System.out.println("ss = " + ss + "  s = " + s);
 
+                    Rectangle r = rects[i].getIntersection(rects[j]);
+                    sMinus += r.getSquare();
+                    inters.add(r);
+                }
             }
             //System.out.println("s = " + s);
         }
-        s = s - ss;
-        System.out.println("Ïëîùàäü îáúåäèíåíèÿ - " + s);
+
+        for (int i = 0; i < inters.size(); i++) {
+            for(int j = i + 1; j < inters.size(); j++) {
+
+                if (j != i) {
+
+                    Rectangle r = inters.get(i).getIntersection(inters.get(j));
+                    sPlus += r.getSquare();
+                }
+            }
+            //System.out.println("s = " + s);
+        }
+        s = s - sMinus + sPlus;
+        System.out.println("Площадь пересечения - " + s);
     }
 }

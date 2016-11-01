4e51206e546787e9603601e6083f6c00cc60806e
ivadimn <ivadimn@mail.ru>
2016-11-01 23:33:27 +0300

    новые версии

---
+++ Lesson21/src/ru/ivadimn/lesson21/ui/GameCircle.java
@@ -0,0 +1,24 @@
+package ru.ivadimn.lesson21.ui;
+
+import javax.swing.*;
+
+
+
+/**
+ * Created by vadim on 01.11.16.
+ */
+public class GameCircle extends JFrame {
+
+    public static void main(String[] args) {
+        new GameCircle();
+    }
+
+    public GameCircle() {
+        setTitle("Кружки");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocation(200,  200);
+        setSize(800, 600);
+        setResizable(false);
+        setVisible(true);
+    }
+}
---
+++ Lesson21/src/ru/ivadimn/lesson21/ui/Main.java
@@ -0,0 +1,7 @@
+package ru.ivadimn.lesson21.ui;
+
+/**
+ * Created by vadim on 01.11.16.
+ */
+public class Main {
+}

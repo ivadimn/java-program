6d364ecbdfdcfd4b3e96a9fd041c2b0f902082f1
ivadimn <ivadimn@mail.ru>
2016-11-11 16:52:21 +0300

    новый проект

---
+++ ChatClient/src/ru/ivadimn/chatclient/Main.java
@@ -0,0 +1,10 @@
+package ru.ivadimn.chatclient;
+
+/**
+ * Created by vadim on 11.11.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ChatClient/src/ru/ivadimn/chatclient/ui/ClientGUI.java
@@ -0,0 +1,21 @@
+package ru.ivadimn.chatclient.ui;
+
+import javax.swing.*;
+
+/**
+ * Created by vadim on 11.11.16.
+ */
+public class ClientGUI extends JFrame {
+
+    private final String CHAT_CLIENT = "Chat Client";
+
+    public ClientGUI() {
+        setTitle(CHAT_CLIENT);
+
+
+        setVisible(true);
+    }
+
+    //private void setLocation()
+
+}
